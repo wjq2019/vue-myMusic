f62fc8a73f369fcedbac63615424a47a54738571
wangjq <892462352@qq.com>
2020-04-09 12:34:29 +0800

    第一次提交代码

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    推荐页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {getRecommend} from 'api/recommend'
+  import { ERR_OK } from 'api/config'
+  export default {
+    created () {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend () {
+        getRecommend().then((res) => {
+          console.log(res)
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
